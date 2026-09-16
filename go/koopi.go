bba9e23b9321fd3f53cb1cdb351ec96876a9becf
Fred Brooker <git@gscloud.cz>
2026-09-16 18:00:36 +0200

    automatic update

--- go/koopi.go
+++ go/koopi.go
@@ -527,9 +527,9 @@ func extractGoodsFromHtml(doc *goquery.Document, category string, query string, 
 			// club
 			newGoods.Club = strings.TrimSpace(offer.Find(".discounts_club").Text())
 			newGoods.Club = strings.ToLower(newGoods.Club)
-			newGoods.Club = strings.ReplaceAll(newGoods.Club, "platí pro členy klubu", "pro členy klubu")
+			newGoods.Club = strings.ReplaceAll(newGoods.Club, "platí pro členy klubu", "pouze pro členy klubu")
 			newGoods.Club = strings.ReplaceAll(newGoods.Club, "cena s aplikací lidl plus", "aplikace Lidl Plus 📱")
-			newGoods.Club = strings.ReplaceAll(newGoods.Club, "cena s kaufland card", "Kaufland Card 💳️")
+			newGoods.Club = strings.ReplaceAll(newGoods.Club, "cena s kaufland card xtra", "Kaufland Card XTRA 💳️")
 			newGoods.Club = sanitizeString(newGoods.Club)
 
 			// validity
